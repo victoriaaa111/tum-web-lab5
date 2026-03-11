2f5dc1b8bca768e5e0a9228a0e467f1649d078b8
victoriaaa111 <victoria.mutruc@gmail.com>
2026-03-11 17:54:49 +0200

    Add go2web.py file for cli program. Make -h (help) command work. Add executable file go2web

---
+++ go2web.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import argparse
+import sys
+
+
+def print_help():
+    print("""go2web - a simple HTTP client
+
+Usage:
+  go2web -u <URL>          Make an HTTP request to the URL and print the response
+  go2web -s <search-term>  Search the term and print top 10 results
+  go2web -h                Show this help message
+""")
+
+
+def main():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('-u', metavar='URL', help='URL to fetch')
+    parser.add_argument('-s', metavar='TERM', nargs='+', help='Search term')
+    parser.add_argument('-h', action='store_true', help='Show help')
+
+    args = parser.parse_args()
+
+    if args.h or len(sys.argv) == 1:
+        print_help()
+        return
+
+    if args.u:
+        print(f"[TODO] Fetching URL: {args.u}")
+        return
+
+    if args.s:
+        term = ' '.join(args.s)
+        print(f"[TODO] Searching for: {term}")
+        return
+
+
+if __name__ == '__main__':
+    main()
